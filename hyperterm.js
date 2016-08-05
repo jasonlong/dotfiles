6b6af357d4bfa22d450f5a440e2235712073be2f
Jason Long <jlong@github.com>
2016-08-05 18:04:25 -0400

    Add hyperterm config

---
+++ hyperterm.js
@@ -0,0 +1,72 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 15,
+
+    // font family with optional fallbacks
+    // fontFamily: '"Operator Mono"',
+    // fontFamily: '"SFMono-Medium"',
+    fontFamily: '"Hack"',
+
+    fontWeight: 300,
+
+    // terminal cursor background color (hex)
+    cursorColor: '#F81CE5',
+
+    // color of the text
+    foregroundColor: '#fff',
+
+    // terminal background color
+    backgroundColor: '#000',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: `
+      x-row {
+        line-height: 1;
+      }
+    `,
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '8px 14px',
+
+    // some color overrides. see http://bit.ly/29k1iU2 for
+    // the full list
+    colors: [
+      '#000000',
+      '#ff0000',
+      '#33ff00',
+      '#ffff00',
+      '#0066ff',
+      '#cc00ff',
+      '#00ffff',
+      '#d0d0d0',
+      '#808080',
+      '#ff0000',
+      '#33ff00',
+      '#ffff00',
+      '#0066ff',
+      '#cc00ff',
+      '#00ffff',
+      '#ffffff'
+    ]
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hypersolar`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: ['hyperterm-lavalamp'],
+
+  // in development, you can create a directory under
+  // `~/.hyperterm_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+};
