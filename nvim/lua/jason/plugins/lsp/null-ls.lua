d79c853cc189e04a7d8105f040a4abe0ce142acb
Jason Long <jason@jasonlong.me>
2023-03-04 14:37:35 -0500

    Add astro to treesitter

--- nvim/lua/jason/plugins/lsp/null-ls.lua
+++ nvim/lua/jason/plugins/lsp/null-ls.lua
@@ -18,7 +18,7 @@ null_ls.setup({
 		formatting.prettier, -- js/ts formatter
 		formatting.stylua, -- lua formatter
 		diagnostics.eslint_d.with({ -- js/ts linter
-			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			-- only enable eslint if root has .eslintrc.js
 			condition = function(utils)
 				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
 			end,
--- nvim/lua/jason/plugins/treesitter.lua
+++ nvim/lua/jason/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = {
+		"astro",
 		"html",
 		"css",
 		"markdown",
