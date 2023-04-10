6840f3a1d2617e203964ffda03fe5f4afdd8d321
Jason Long <jason@jasonlong.me>
2023-04-10 13:35:35 -0400

    Add space invaders dashboard hero

---
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -0,0 +1,16 @@
+return {
+  "goolord/alpha-nvim",
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+    local logo = [[
+                                                                    
+     ▀▄   ▄▀        ▄ ▀▄   ▄▀ ▄       ▄▄████▄▄           ▄██▄       
+    ▄█▀███▀█▄       █▄███████▄█      ██████████        ▄██████▄     
+   █▀███████▀█      ███▄███▄███      ██▄▄██▄▄██       ███▄██▄███    
+   █ █▀▀▀▀▀█ █      ▀█████████▀       ▄▀▄▀▀▄▀▄          ▄▀▄▄▀▄      
+      ▀▀ ▀▀          ▄▀     ▀▄       ▀        ▀        ▀ ▀  ▀ ▀     
+]]
+    dashboard.section.header.val = vim.split(logo, "\n")
+    return dashboard
+  end,
+}
