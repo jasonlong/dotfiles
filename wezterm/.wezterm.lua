70833794be29d0ffdefc327f72d14ca754627533
Jason Long <jason@jasonlong.me>
2023-04-10 08:01:24 -0400

    Use integrated macos window controls

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,8 +17,10 @@ config.line_height = 1.1
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
--- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_decorations = "RESIZE"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.macos_window_background_blur = 40
+config.window_background_opacity = 0.90
+config.window_close_confirmation = "NeverPrompt"
 
 config.window_padding = {
 	left = "24px",
