b0bb039c6769620bd1985631223c15725d253fa4
Jason Long <jason@jasonlong.me>
2023-04-10 08:01:04 -0400

    Use transparent bg so wezterm transparency works

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
     lazy = true,
     opts = {
       style = "moon",
+      transparent = true,
       styles = {
         comments = { italic = false },
         keywords = { italic = false },
