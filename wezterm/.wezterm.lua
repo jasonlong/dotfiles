eb2d721a581d60135d0ec8f6997dfe71ad26d42f
Jason Long <jason@jasonlong.me>
2023-04-21 20:37:47 -0400

    Use Berkeley Mono font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,19 +1,15 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
 config.color_scheme = "tokyonight_moon"
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { italic = false })
+config.font = wezterm.font("BerkeleyMono Nerd Font", { italic = false })
 config.font_size = 15.0
-config.line_height = 1.1
+config.line_height = 1.15
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
