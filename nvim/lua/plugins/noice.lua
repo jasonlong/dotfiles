7f5e19753b4cc654f2513a3978609b55c0617c63
Jason Long <jason@jasonlong.me>
2024-01-29 09:08:48 -0500

    Add configs for noice and nvim-ufo

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+}
---
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,37 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = { "kevinhwang91/promise-async" },
+  event = "BufRead",
+  keys = {
+    {
+      "zR",
+      function()
+        require("ufo").openAllFolds()
+      end,
+    },
+    {
+      "zM",
+      function()
+        require("ufo").closeAllFolds()
+      end,
+    },
+    {
+      "K",
+      function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end,
+    },
+  },
+  config = function()
+    vim.o.foldcolumn = "1"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    require("ufo").setup({
+      close_fold_kinds = { "imports" },
+    })
+  end,
+}
