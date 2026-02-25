ef022956f583f960037d59121731b09111fce4ad
Jason Long <jason@jasonlong.me>
2026-02-25 18:04:20 -0500

    Update Brewfile when updating brews

--- fish/functions/bb.fish
+++ fish/functions/bb.fish
@@ -1,4 +1,5 @@
 # Defined via `source`
 function bb --wraps='brew update; brew upgrade' --description 'alias bb=brew update; brew upgrade'
-  brew update; brew upgrade; brew cleanup $argv;
+  brew update; brew upgrade; brew cleanup $argv
+  brew bundle dump --file=~/dev/dotfiles/Brewfile --describe --force
 end
